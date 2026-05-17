958928456107e9196ce6f2346654165d3d0e97fc
kerolesmonsef <keroles.monsef@foo.mobi>
2026-05-18 00:56:13 +0300

    Update application properties and LoadBalancerConfig for service discovery

--- LoadBalancer/src/main/java/com/keroles/loadbalancer/LoadBalancerConfig.java
+++ LoadBalancer/src/main/java/com/keroles/loadbalancer/LoadBalancerConfig.java
@@ -1,13 +1,11 @@
 package com.keroles.loadbalancer;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@LoadBalancerClient(value = "myservice", configuration = MyServiceConfig.class)
 public class LoadBalancerConfig {
 
     @Bean
