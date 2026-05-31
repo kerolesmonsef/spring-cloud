1e863e86a18faba03112da327157e2202c006006
kerolesmonsef <keroles.monsef@foo.mobi>
2026-05-31 17:28:36 +0300

    Initialize OpenFeign module with basic configuration and REST client for posts

--- OpenFeign/src/main/java/com/keroles/openfeign/PostsClient.java
+++ OpenFeign/src/main/java/com/keroles/openfeign/PostsClient.java
@@ -1,11 +1,13 @@
 package com.keroles.openfeign;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "posts-client")
 public interface PostsClient {
     @GetMapping("/posts")
+    @Retry(name = "posts-client")
     PostResponse getPosts();
 }
 
